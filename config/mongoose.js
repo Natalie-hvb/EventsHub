54d31a4a64abe725cb9bf2bcb2ee26e907dd7838
Natalie HV <114947915+Natalie-hvb@users.noreply.github.com>
2023-09-13 16:59:40 +0200

    Delete config/mongoose.js

--- config/mongoose.js
+++
@@ -1,9 +0,0 @@
-// Connect to MongoDB
-const mongoose = require('mongoose'); 
-
-DB='mongodb+srv://natalie:natalie123@eventshub.8xtiyu9.mongodb.net/?retryWrites=true&w=majority'
-
-mongoose.connect(DB)
-  .then((result) => console.log('connected to the db'))
-  .catch((err) => console.log(err)
-);
