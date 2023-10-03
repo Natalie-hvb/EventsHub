630f1a5a6b632d7a8411d04cd65f411834705692
Natalie HV <114947915+Natalie-hvb@users.noreply.github.com>
2023-10-03 22:45:57 +0200

    Delete config directory

--- config/otherRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const userController = require('../controllers/userController'); // Import user controller
-const route = express.Router();
-
-route.get('/user/:id', userController.getUserById); // Add user router
-
-module.exports = route;
