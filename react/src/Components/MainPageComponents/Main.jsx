aa7d30cda1dc6537adb0096df9815597227e63ec
Burhan <ahmetilov20@gmail.com>
2023-10-04 18:49:17 +0200

    v1.2

--- react/src/Components/MainPageComponents/Main.jsx
+++ react/src/Components/MainPageComponents/Main.jsx
@@ -1,32 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../AuthComponents/AuthContext';
 import './Main.css';
 
 function Main() {
-  const { user } = useContext(AuthContext); // Access user state from AuthContext
-
     return (
         <div>
             <div className='banner'>
                 <h1 className='slogan'>WHERE PEOPLE MEET</h1>
                 <div className='bannerButtons'>
-                    {!user ? (
-                        <>
-                        <Link to="/signup" className='bannerButton signUp'>
-                            Sign Up
-                        </Link>
-                        <Link to="/login" className='bannerButton login'>
-                            Login
-                        </Link>
-                        </>
-                    ) : (
-                        <Link to="/profile" className='bannerButton'>
-                        Profile
-                        </Link>
-                    )}
                 <Link to="/events" className='bannerButton'>
-                    Top Events
+                    Best Events
                 </Link>
                 </div>
             </div>
